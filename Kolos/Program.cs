766df703b5c1997c30815295907e90c774b05042
GTom93 <gradzinskitomasz@gmail.com>
2016-05-16 10:40:23 +0200

    commit całościowy, wszystko

---
+++ Kolos/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolos
+{
+   public class Program
+    {
+       /// <summary>
+       /// metoda sprawdzająca czy liczba jest półpierwszą
+       /// </summary>
+       /// <param name="c"> liczba którą chcemy sprawdzić </param>
+       /// <returns> zwraca prawdę jesli liczba jest półpierwszą</returns>
+       public static  bool IsSemiPrime( int c )
+        {
+             int a = 2, b = 0;
+             while( b < 3 && c != 1 )
+             {
+	            if( ( c % a )!=0 ) 
+	            { c /= a; b++; }
+	            else a++;
+             }
+            return b==2 ;
+        }
+        /// <summary>
+        /// metoda wyszukująca z podanych zakresów liczby półpierwsze
+        /// </summary>
+        /// <param name="N">najwieksza liczba w tabeli</param>
+        /// <param name="P"> tablica  zawierająca początkowe zakresy sprawdzanych przedziałów</param>
+        /// <param name="Q">tablica  zawierająca kńcowe zakresy sprawdzanych przedziałów</param>
+        /// <param name="M"> liczba  zakresów do sprawdzenia</param>
+        /// <returns> tablica  zawirająca w kolejności z każdego zakresu ile jest w nim liczb półpierwszych</returns>
+        public static int[] SemiPrime(int N, int[] P, int[] Q, int []M)
+        {       
+            for (int i = 0; i < P.Length; i++ )
+            {
+                int left  = P[i];
+                int right = Q[i];
+                int count=0;
+                for (int j=left; j<=right; j++)
+                {
+                    if (IsSemiPrime(j) == true) count++; 
+                }
+                M[i] = count; 
+            }
+                return M;
+        }
+        static void Main(string[] args) { }
+    }
+}
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kolos;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        /// <summary>
+        /// test czy funkcja poprawnie wykryje liczby półpierwsze
+        /// </summary>
+        [TestMethod]
+        public void TestSemiPrime()
+        {
+            int[] P = new int[] { 1, 4, 16 };
+            int[] Q = new int[] { 26, 10, 20 };
+            int N = 26;
+            int[] M = new int[3];
+            int[] expection = new int[] { 10, 4, 0 };
+
+            Kolos.Program.SemiPrime(N, P, Q, M);
+            
+            for (int i=0; i<P.Length-1; i++)
+            {
+                Assert.AreEqual(expection[i], M[i]);
+            }     
+        }
+
+        /// <summary>
+        /// test czy tablica wyjściowa M nie jest pusta
+        /// </summary>
+        [TestMethod]
+        public void NullTest()
+        {
+            int[] P = new int[] { 1};
+            int[] Q = new int[] { 5};
+            int N = 5;
+            int[] M = new int[1];
+
+            Kolos.Program.SemiPrime(N, P, Q, M);
+            Assert.IsNotNull(M);
+        }
+
+        /// <summary>
+        /// test czy funkcja pomocnicza prawidłowo weryfikuje czy
+        /// liczba jest liczbą półpierwszą
+        /// </summary>
+        [TestMethod]
+        public void IsSemiPrimeTest()
+        {
+            Assert.IsTrue(Kolos.Program.IsSemiPrime(10));
+            Assert.IsFalse(Kolos.Program.IsSemiPrime(7));
+        }
+    }
+}
